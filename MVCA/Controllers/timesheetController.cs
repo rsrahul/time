076e7035a5418c60650a1d616d9a6980be098438
Rahul Srivastav <rsrahul99@gmail.com>
2013-07-24 18:21:21 +0530

    New Changes
    Delete option, index page design

--- MVCA/Controllers/timesheetController.cs
+++ MVCA/Controllers/timesheetController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace MVCA.Controllers
 {
@@ -40,6 +41,33 @@ namespace MVCA.Controllers
         return View();
       }
         }
+
+        public ActionResult Delete(int id)
+        
+        {
+            using (var db = new Timesheet1()) 
+            {
+                return View(db.entry.Find(id)); 
+            }
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, Timesheet sheet) 
+        {
+            try
+            {
+                using (var db = new Timesheet1()) 
+                {
+                    db.Entry(sheet ).State = System.Data.Entity. EntityState.Deleted;
+                    db.SaveChanges();
+                } 
+                return RedirectToAction("Index");
+            }
+            catch 
+            {
+                return View();
+            }
+        }
+
         //[HttpPost]
        // public ActionResult Index(timesheet sheet)
         //{
